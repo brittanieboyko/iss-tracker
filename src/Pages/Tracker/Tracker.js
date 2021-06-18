85751216e9e2b1eb851736106e94532da01b7e3d
Brittanie <brittanie.boyko@gmail.com>
2021-06-18 09:16:35 -0400

    set issPosition state to current latitude and longitude of iss from api data

--- src/Pages/Tracker/Tracker.js
+++ src/Pages/Tracker/Tracker.js
@@ -2,7 +2,7 @@ import {React, useState } from 'react';
 import axios from 'axios';
 
 const Tracker = () => {
-    let [responseData, setResponseData] = useState('')
+    let [issPosition, setIssPosition] = useState('')
 
     const fetchData = (e) => {
         e.preventDefault()
@@ -12,23 +12,16 @@ const Tracker = () => {
             'url':'http://api.open-notify.org/iss-now.json',
         })
         .then((response)=>{
-            setResponseData(response.data)
-            console.log(response)
+            setIssPosition(response.data.data);
         })
         .catch((error) => {
             console.log(error)
         })
     }
 
-
     return (
         <div>
-
             <button onClick={(e) => fetchData(e)} type='button'>Click Me For Data</button>
-
-            {/* {responseData.dates && responseData.dates.map(date => {
-                return <p>{date}</p>
-            })} */}
         </div>
     )
 }
