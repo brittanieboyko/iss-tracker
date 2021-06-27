3494699e30975f04547bbe37541e7c8d0bf49d24
Brittanie <brittanie.boyko@gmail.com>
2021-06-27 09:56:59 -0400

    set setIssPosition state to correct iss coordinates from iss response data

--- src/Pages/Tracker/Tracker.js
+++ src/Pages/Tracker/Tracker.js
@@ -1,27 +1,29 @@
-import {React, useState } from 'react';
-import {getIssLocation} from '../../API/api';
+import { React, useState } from "react";
+import { getIssLocation } from "../../API/api";
 
 const Tracker = () => {
-    let [issPosition, setIssPosition] = useState('')
+  let [issPosition, setIssPosition] = useState("");
 
-    const fetchData = (e) => {
-        e.preventDefault()
+  const fetchData = (e) => {
+    e.preventDefault();
 
-        getIssLocation()
-        .then((response)=>{
-            setIssPosition(response.data.data);
-            console.log(response);
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-    }
+    getIssLocation()
+      .then((response) => {
+        setIssPosition(response.data);
+        console.log(issPosition);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
-        <div>
-            <button onClick={(e) => fetchData(e)} type='button'>Click Me For Data</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <button onClick={(e) => fetchData(e)} type="button">
+        Click Me For Data
+      </button>
+    </div>
+  );
+};
 
 export default Tracker;
