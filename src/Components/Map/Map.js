c38a26ba251a38a923817246cd1607b341c34a57
Brittanie <brittanie.boyko@gmail.com>
2021-07-16 22:50:23 -0400

    imported custom map style json and added it as a prop in map component

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,6 +1,7 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import MapMarker from "../MapMarker/MapMarker";
+import mapStyle from "../../assets/MapStyles";
 
 const Map = ({ latlon }) => {
   const defaultProps = {
@@ -9,19 +10,20 @@ const Map = ({ latlon }) => {
       lng: parseFloat(latlon.longitude) || 0,
     },
     zoom: 3,
+    styles: mapStyle,
   };
-
+  console.log(defaultProps.styles.mapStyle);
   return (
     <div style={{ height: "90vh", width: "90%" }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API }}
         center={defaultProps.center}
         defaultZoom={defaultProps.zoom}
+        options={{
+          styles: defaultProps.styles.mapStyle,
+        }}
       >
-        <MapMarker
-          lat={latlon.latitude}
-          lng={latlon.longitude}
-        />
+        <MapMarker lat={latlon.latitude} lng={latlon.longitude} />
       </GoogleMapReact>
     </div>
   );
