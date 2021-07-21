5f2740d77a1ed6e51084b8b298165f82b34ef229
Brittanie <brittanie.boyko@gmail.com>
2021-07-21 10:37:41 -0400

    removed zoom controls from mac

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -14,19 +14,20 @@ const Map = ({ latlon }) => {
   };
   console.log(defaultProps.styles.mapStyle);
   return (
-    <div style={{ height: "90vh", width: "100%" }}>
+    <div style={{ height: "80vh", width: "100%" }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API }}
         center={defaultProps.center}
         defaultZoom={defaultProps.zoom}
         options={{
           styles: defaultProps.styles.mapStyle,
+          zoomControl:false
         }}
       >
         <MapMarker lat={latlon.latitude} lng={latlon.longitude} />
       </GoogleMapReact>
       <h1>ISS TRACKER</h1>
-      <h2>`lat: {defaultProps.center.lat} long: {defaultProps.center.lng}`</h2>
+      <h2>lat: {defaultProps.center.lat} long: {defaultProps.center.lng}</h2>
     </div>
   );
 };
