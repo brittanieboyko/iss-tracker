2330b137fc4b675d838a5a926d850730e2c1968d
Brittanie <brittanie.boyko@gmail.com>
2021-07-28 21:13:35 -0400

    added new api call for isslocation over https

--- src/API/api.js
+++ src/API/api.js
@@ -3,5 +3,8 @@ import axios from "axios";
 const getIssLocation = () => {
   return axios.get("http://api.open-notify.org/iss-now.json");
 };
+const getIssLocationSecure = () => {
+  return axios.get("https://api.wheretheiss.at/v1/satellites/25544");
+};
 
-export { getIssLocation };
+export { getIssLocation, getIssLocationSecure };
